6672c814be732a8a62d103a8f5d7551c4c8eb485
Savi8 <punkyyyland@hotmail.com>
2017-06-14 19:13:24 +0300

    Create plot4.R

--- plot4.R
+++ plot4.R
@@ -1,32 +1,14 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# merge the two data sets 
-if(!exists("NEISCC")){
-  NEISCC <- merge(NEI, SCC, by="SCC")
-}
-
-library(ggplot2)
-
-# Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
-
-# fetch all NEIxSCC records with Short.Name (SCC) Coal
-coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
-subsetNEISCC <- NEISCC[coalMatches, ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
-
-
-
-png("plot4.png", width=640, height=480)
-g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
-g <- g + geom_bar(stat="identity") +
-  xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from coal sources from 1999 to 2008')
-print(g)
+## Reading in the data 
+NEI <- readRDS("summarySCC_PM25.rds") 
+SCC <- readRDS("Source_Classification_Code.rds") 
+## Finding only the coal related observations summary(SCC$Short.Name) 
+# grepl returns a locial vector 
+SCC_coal <- SCC[grepl("Coal", SCC$Short.Name, ignore.case = TRUE),] 
+NEI_coal <- subset(NEI, NEI$SCC %in% SCC_coal$SCC) 
+## Total emissions over each year 
+NEI_coal_total <- with(NEI_coal, tapply(Emissions, year, sum)) 
+## plot 
+png(filename = "plot4.png") 
+plot(NEI_coal_total, xaxt="n", xlab = "Year", ylab = "PM 2.5 (tons)", main = "Total Emissions from coal combustion-related sources across the US ", type = "b", col = "blue") 
+axis(1, 1:4, c("1999", "2002", "2005", "2008")) 
 dev.off()
