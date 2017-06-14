6b2bd6746b62000acbbe2bc497209e465259196e
Savi8 <punkyyyland@hotmail.com>
2017-06-14 19:19:07 +0300

    Create plot6.R

--- plot6.R
+++ plot6.R
@@ -1,36 +1,16 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# merge the two data sets 
-if(!exists("NEISCC")){
-  NEISCC <- merge(NEI, SCC, by="SCC")
-}
-
-library(ggplot2)
-
-# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
-# vehicle sources in Los Angeles County, California (fips == "06037"). 
-# Which city has seen greater changes over time in motor vehicle emissions?
-
-# 24510 is Baltimore, see plot2.R, 06037 is LA CA
-# Searching for ON-ROAD type in NEI
-# Don't actually know it this is the intention, but searching for 'motor' in SCC only gave a subset (non-cars)
-subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
-
-aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
-aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
-aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
-
-png("plot6.png", width=1040, height=480)
-g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
-g <- g + facet_grid(. ~ fips)
-g <- g + geom_bar(stat="identity")  +
-  xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
-print(g)
+## Reading in the data 
+NEI <- readRDS("summarySCC_PM25.rds") 
+SCC <- readRDS("Source_Classification_Code.rds") 
+## Subsets for Baltimore City and Los Angeles County with type == "ON-ROAD" 
+vec_bc_tot <- aggregate(Emissions ~ year, subset(NEI, fips == "24510" & type == "ON-ROAD"), sum) 
+vec_lac_tot <- aggregate(Emissions ~ year, subset(NEI, fips == "06037" & type == "ON-ROAD"), sum) 
+## plot 
+png(filename = "plot6.png", width = 1000) par(mfrow = c(1,2)) 
+plot(vec_bc_tot, type = "l", lwd = 2, col = "red", xlab = "Year", ylab = "PM 2.5 (tons)", main = "Baltimore City") 
+legend("topright", c("vehicle sources (BC)"), 
+lty = 1, col = "red", cex = 1) 
+plot(vec_lac_tot, type = "l", lwd = 2, col = "blue", xlab = "Year", ylab = "PM 2.5 (tons)", 
+main = "Los Angeles County") 
+legend("topleft",
+c("vehicle sources (LAC)"), lty = 1, col = "blue", cex = 1) 
 dev.off()
